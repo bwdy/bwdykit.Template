5aa2f8e9036d779bb4d6072d2b521ac9744a7f6d
bwdy <47769190+bwdymods@users.noreply.github.com>
2020-07-10 22:46:58 -0800

    Add AppInfo

---
+++ Game/AppInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    public static class AppInfo
+    {
+        public const string Title = "Templatey McTemplateface";
+        public const string Description = "A template for all your templating needs.";
+        public const string Version = "1.0.0";
+
+        public const string Developer = "Hotdog McGillicutty";
+        public const string CopyrightYear = "2020";
+    }
+}
